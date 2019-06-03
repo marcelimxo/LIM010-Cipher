f334f35346fabe2e0a2c8e57b84c993de00ed1b6
marcelimxo <marcelimrondon@gmail.com>
2019-06-03 12:54:51 -0500

    Empezando a implementar la funcion del login

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-/* Acá va tu código */
+const checkLogin = document.getElementById('checkLogin')
+
+/*aprender esta funcion*/
+function check(form)   /*function to check userid & password*/
+{
+ /*the following code checkes whether the entered userid and password are matching*/
+                             
+ if(form.pswrd.value == "mmm")
+  {
+    window.open('target.html')
+  /*opens the target page while Id & password matches*/
+  }
+ else
+ {
+   alert("Error Password or Username")/*displays error message*/
+  }
+}
