8cc83a1edf756b1c8b8ff1489b346e166162fbeb
marcelimxo <marcelimrondon@gmail.com>
2019-06-03 11:50:55 -0500

    Agregue fuentes al css y html

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,10 @@
 window.cipher = {
   encode: () => {
+    /*desplazamiento hacia la derecha*/
     /* Acá va tu código */
   },
   decode: () => {
+    /*desplazamiento hacia la izquierda*/
     /* Acá va tu código */
   }
 };
