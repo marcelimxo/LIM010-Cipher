fcf8a7e002a9e561979b401c6cd8755f173646ff
marcelimxo <marcelimrondon@gmail.com>
2019-06-05 16:58:10 -0500

    Primer intento de la funcion encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,21 @@
 window.cipher = {
-  encode: () => {
+  encode: (offset,string) => {
+
+    let solved = '';
+
+    for(let i=0; i<string.length; i++){
+
+      const asciiNum = string[i].charCodeAt();
+      if(asciiNum >= 65 && asciiNum <= 90){
+        solved+= String.fromCharCode(asciiNum + offset)
+      }
+
+    } return solved;
+
     /*desplazamiento hacia la derecha*/
     /* Acá va tu código */
   },
-  decode: () => {
+  decode: (offset,string) => {
     /*desplazamiento hacia la izquierda*/
     /* Acá va tu código */
   }
