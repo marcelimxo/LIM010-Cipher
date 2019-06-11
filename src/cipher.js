976f2da30ad8baad399b722ec63b247d64c1af74
marcelimxo <marcelimrondon>
2019-06-11 02:57:22 -0500

    Estilos :-)

--- src/cipher.js
+++ src/cipher.js
@@ -1,53 +1,45 @@
 window.cipher = {
-  encode: (offset, string) => {
-    let solved = '';
-    const stringArr = string.split('');
-    stringArr.forEach((letra) => {
-      const asciiNum = letra.charCodeAt();
-      switch (true) {
-        case (asciiNum >= 65 && asciiNum <= 90):
-          {
-            const code = (asciiNum - 65 + parseInt(offset)) % 26 + 65;
-            solved += String.fromCharCode(code);
-            break;
-          }
-        case (asciiNum >= 97 && asciiNum <= 122):
-          {
-            const code = (asciiNum - 97 + parseInt(offset)) % 26 + 97;
-            solved += String.fromCharCode(code);
-            break;
-          }
-        case (asciiNum === 32):
-          {
-            solved += ' '
-            break;
-          }
-        default:
-          solved;
-      } 
-    });
-    return solved;
-  },
-  decode: (offset, string) => {
-    let solved = '';
-    const stringArr = string.split('');
-    stringArr.forEach((letra) => {
-      const asciiNum = letra.charCodeAt();
-      if (asciiNum >= 65 && asciiNum <= 90) {
-        const code = (asciiNum + 65 - parseInt(offset)) % 26 + 65;
-        solved += String.fromCharCode(code);
-
-      } else if (asciiNum >= 97 && asciiNum <= 122) {
-        const code = (asciiNum + 97 + parseInt(offset)) % 26 + 97;
-        solved += String.fromCharCode(code)
-
-        console.log('Este es el solved ' + solved,'este es el code' + code)
-
-      } else if (asciiNum === 32) {
-
-        solved += ' '
-      }
-    });
-    return solved;
-  }
-}
+	encode: (offset, string) => {
+		let solved = '';
+		const stringArr = string.split('');
+		stringArr.forEach((letra) => {
+			const asciiNum = letra.charCodeAt();
+			switch (true) {
+				case asciiNum >= 65 && asciiNum <= 90: {
+					const code = (asciiNum - 65 + parseInt(offset)) % 26 + 65;
+					solved += String.fromCharCode(code);
+					break;
+				}
+				case asciiNum >= 97 && asciiNum <= 122: {
+					const code = (asciiNum - 97 + parseInt(offset)) % 26 + 97;
+					solved += String.fromCharCode(code);
+					break;
+				}
+				case asciiNum === 32: {
+					solved += ' ';
+					break;
+				}
+				default:
+					solved;
+			}
+		});
+		return solved;
+	},
+	decode: (offset, string) => {
+		let solved = '';
+		const stringArr = string.split('');
+		stringArr.forEach((letra) => {
+			const asciiNum = letra.charCodeAt();
+			if (asciiNum >= 65 && asciiNum <= 90) {
+				const code = (asciiNum + 65 - parseInt(offset)) % 26 + 65;
+				solved += String.fromCharCode(code);
+			} else if (asciiNum >= 97 && asciiNum <= 122) {
+				const code = (asciiNum + 97 + parseInt(offset)) % 26 + 97;
+				solved += String.fromCharCode(code);
+			} else if (asciiNum === 32) {
+				solved += ' ';
+			}
+		});
+		return solved;
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,64 +1,70 @@
 //const checkLogin = document.getElementById('checkLogin');
 const pswrd = document.getElementById('pswrd');
-const name = document.getElementById('name');
 const form = document.getElementById('form');
 const correct_pswrd = 'LABORATORIA';
 
 //contador de errores
 let fails = 0;
 const check = (e) => {
-  //con preventDefault evito que el form haga cosas por defecto en el submit, como por ejemplo, agregar los valores en la url
-  e.preventDefault()
-  if (pswrd.value.length > 0) {
-    if (pswrd.value === correct_pswrd) {
-      const loginContainer = document.getElementById('loginContainer')
-      loginContainer.remove();
-      const description = document.getElementById('description')
-      description.classList.add('hide')
-      const opt = document.getElementById('op')
-      opt.classList.replace('hide', 'show')
-    } else {
-      pswrd.classList.add('error')
+	//con preventDefault evito que el form haga cosas por defecto en el submit, como por ejemplo, agregar los valores en la url
+	e.preventDefault();
+	if (pswrd.value.length > 0) {
+		if (pswrd.value === correct_pswrd) {
+			const loginContainer = document.getElementById('loginContainer');
+			loginContainer.remove();
+			const description = document.getElementById('description');
+			description.classList.add('hide');
+			const opt = document.getElementById('op');
+			opt.classList.replace('hide', 'show');
+		} else {
+			pswrd.classList.add('error');
 
-      if (fails < 2) {
-        // si tiene menos de 3 intentos entra aqui
-        
-        const error_pswrd = document.getElementById('error_pswrd');
-            error_pswrd.classList.replace('hide', 'show');
+			if (fails < 2) {
+				// si tiene menos de 3 intentos entra aqui
 
-        // borramos la data del input
-        pswrd.value = ''
+				const error_pswrd = document.getElementById('error_pswrd');
+				error_pswrd.classList.replace('hide', 'show');
 
-        // sumamos +1 al contador de errores
-        fails++
+				// borramos la data del input
+				pswrd.value = '';
 
-      } else {
-        // al tercer fallo entra aqui
-        const modal = document.getElementById('error')
-        const overlay = document.getElementById('overlay')
+				// sumamos +1 al contador de errores
+				fails++;
+			} else {
+				// al tercer fallo entra aqui
+				const modal = document.getElementById('error');
+				const overlay = document.getElementById('overlay');
 
-        modal.classList.replace('hide', 'show')
-        overlay.classList.replace('hide', 'show')
-      }
-    }
-  }
-}
+				modal.classList.replace('hide', 'show');
+				overlay.classList.replace('hide', 'show');
+			}
+		}
+	}
+};
 
 form.addEventListener('submit', check);
 
 //opciones
 const options = document.getElementById('options');
 options.addEventListener('change', (e) => {
-  const encodeSection = document.getElementById('cifrando');
-  const decodeSection = document.getElementById('descifrando');
-  if(e.target.value === 'Cifrar'){
-    encodeSection.classList.replace('hide', 'show');
-    decodeSection.classList.replace('show','hide');    
-   } else if(e.target.value === 'Descifrar'){
-    decodeSection.classList.replace('hide', 'show');
-    encodeSection.classList.replace('show','hide');   
-   }
-  });
+	const encodeSection = document.getElementById('cifrando');
+	const decodeSection = document.getElementById('descifrando');
+	if (e.target.value === 'Cifrar') {
+		encodeSection.classList.replace('hide', 'show');
+		decodeSection.classList.replace('show', 'hide');
+		textoCifrar.value = '';
+		textoResultadoCifrar.value = '';
+		rangeCifrar.value = 1;
+		fieldCifrar.value = 1;
+	} else if (e.target.value === 'Descifrar') {
+		decodeSection.classList.replace('hide', 'show');
+		encodeSection.classList.replace('show', 'hide');
+		textoDescifrar.value = '';
+		textoResultadoDescifrar.value = '';
+		rangeDescifrar.value = 1;
+		fieldDescifrar.value = 1;
+	}
+});
 
 // sección cifrar
 
@@ -68,10 +74,10 @@ const rangeCifrar = document.querySelector('#rangeCifrar');
 const fieldCifrar = document.getElementById('numCifrar');
 
 rangeCifrar.addEventListener('input', (e) => {
-  fieldCifrar.value = e.target.value;
+	fieldCifrar.value = e.target.value;
 });
 fieldCifrar.addEventListener('input', (e) => {
-  rangeCifrar.value = e.target.value;
+	rangeCifrar.value = e.target.value;
 });
 
 /* Cifrando y mostrando */
@@ -81,30 +87,29 @@ const textoResultadoCifrar = document.getElementById('textoResultadoCifrar');
 
 const btnCifrar = document.getElementById('cifrar');
 btnCifrar.addEventListener('click', () => {
-  const textoCifrado = window.cipher.encode(fieldCifrar.value,textoCifrar.value);  
-  textoResultadoCifrar.value = textoCifrado;
-})
+	const textoCifrado = window.cipher.encode(fieldCifrar.value, textoCifrar.value);
+	textoResultadoCifrar.value = textoCifrado;
+});
 
 /* reset en cifrar */
 
 const btnResetCifrar = document.getElementById('resetCifrar');
-btnResetCifrar.addEventListener('click', ()=> {
-  textoCifrar.value = '';
-  rangeCifrar.value = 1;
-  fieldCifrar.value = 1;
-} )
+btnResetCifrar.addEventListener('click', () => {
+	textoCifrar.value = '';
+	rangeCifrar.value = 1;
+	fieldCifrar.value = 1;
+});
 
 /* copiar en cifrar */
 
 const btnCopiarCifrado = document.getElementById('copiarCifrado');
 btnCopiarCifrado.addEventListener('click', () => {
-  textoResultadoCifrar.select();
-  document.execCommand('Copy');
+	textoResultadoCifrar.select();
+	document.execCommand('Copy');
 
-  const copiar = document.getElementById('copiarCifrado_span');
-  copiar.classList.toggle('hide');
-
-})
+	const copiar = document.getElementById('copiarCifrado_span');
+	copiar.classList.toggle('hide');
+});
 
 // sección descifrar
 
@@ -114,10 +119,10 @@ const rangeDescifrar = document.querySelector('#rangeDescifrar');
 const fieldDescifrar = document.getElementById('numDescifrar');
 
 rangeDescifrar.addEventListener('input', (e) => {
-  fieldDescifrar.value = e.target.value;
+	fieldDescifrar.value = e.target.value;
 });
 fieldDescifrar.addEventListener('input', (e) => {
-  rangeDescifrar.value = e.target.value;
+	rangeDescifrar.value = e.target.value;
 });
 
 /* Descifrando y mostrando */
@@ -127,30 +132,27 @@ const textoResultadoDescifrar = document.getElementById('textoResultadoDescifrar
 
 const btnDescifrar = document.getElementById('descifrar');
 btnDescifrar.addEventListener('click', () => {
-  const textoDescifrado = window.cipher.decode(fieldDescifrar.value,textoDescifrar.value)
-  
-  textoResultadoDescifrar.value = textoDescifrado;
+	const textoDescifrado = window.cipher.decode(fieldDescifrar.value, textoDescifrar.value);
 
-})
+	textoResultadoDescifrar.value = textoDescifrado;
+});
 
 /* reset en descifrar */
 
 const btnResetDescifrar = document.getElementById('resetDescifrar');
-btnResetDescifrar.addEventListener('click', ()=> {
-  textoDescifrar.value = '';
-  rangeDescifrar.value = 1;
-  fieldDescifrar.value = 1;
-} )
+btnResetDescifrar.addEventListener('click', () => {
+	textoDescifrar.value = '';
+	rangeDescifrar.value = 1;
+	fieldDescifrar.value = 1;
+});
 
 /* copiar en cifrar */
 
 const btnCopiarDescifrado = document.getElementById('copiarDescifrado');
 btnCopiarDescifrado.addEventListener('click', () => {
-  textoResultadoDescifrar.select();
-  document.execCommand('Copy');
+	textoResultadoDescifrar.select();
+	document.execCommand('Copy');
 
-  const copiar = document.getElementById('copiarDescifrado_span');
-  copiar.classList.toggle('hide');
-
-})
-
+	const copiar = document.getElementById('copiarDescifrado_span');
+	copiar.classList.toggle('hide');
+});
