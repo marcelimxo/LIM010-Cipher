4455b1e4c769ed94b4e28524b20e4b1f7ae2d668
marcelimxo <marcelimrondon>
2019-06-11 00:30:19 -0500

    Ya volvi a descifrar :clown_face:

--- src/cipher.js
+++ src/cipher.js
@@ -37,10 +37,12 @@ window.cipher = {
         const code = (asciiNum + 65 - parseInt(offset)) % 26 + 65;
         solved += String.fromCharCode(code);
 
-        console.log('Este es el solved ' + solved,'este es el code' code)
       } else if (asciiNum >= 97 && asciiNum <= 122) {
-        const code = (asciiNum + 97 - parseInt(offset)) % 26 + 97 + 14;
+        const code = (asciiNum + 97 + parseInt(offset)) % 26 + 97;
         solved += String.fromCharCode(code)
+
+        console.log('Este es el solved ' + solved,'este es el code' + code)
+
       } else if (asciiNum === 32) {
 
         solved += ' '
