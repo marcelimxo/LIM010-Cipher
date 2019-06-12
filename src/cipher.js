f321929f37c2eb0b91010e1d65d5e06922eb4710
marcelimxo <marcelimrondon>
2019-06-12 08:41:12 -0500

    ya descifra

--- src/cipher.js
+++ src/cipher.js
@@ -25,7 +25,7 @@ window.cipher = {
 		});
 		return solved;
 	},
-	decode: (offset, string) => {
+	decodeDeprecated: (offset, string) => {
 		let solved = '';
 		const stringArr = string.split('');
 		stringArr.forEach((letra) => {
@@ -34,15 +34,41 @@ window.cipher = {
 				const code = (asciiNum + 65 - parseInt(offset)) % 26 + 65;
 				solved += String.fromCharCode(code);
 			} else if (asciiNum >= 97 && asciiNum <= 111) {
-				const code = ((asciiNum + 97 - parseInt(offset)) % 26 + 97)-12;
+				const code = (asciiNum + 97 - parseInt(offset)) % 26 + 97 - 12;
 				solved += String.fromCharCode(code);
-			} else if (asciiNum >= 111 && asciiNum <= 122){
-				const code = ((asciiNum + 97 - parseInt(offset)) % 26 + 97)-12+26;
+			} else if (asciiNum >= 111 && asciiNum <= 122) {
+				const code = (asciiNum + 97 - parseInt(offset)) % 26 + 97 - 12 + 26;
 				// eslint-disable-next-line no-console
-				console.log('este es el numero ascii ' + asciiNum, 'y ese es el numero de la formula ' + code)
+				console.log('este es el numero ascii ' + asciiNum, 'y ese es el numero de la formula ' + code);
 				solved += String.fromCharCode(code);
 			} else if (asciiNum === 32) {
 				solved += ' ';
+			}
+		});
+		return solved;
+	},
+	decode: (offset, string) => {
+		let solved = '';
+		const stringArr = string.split('');
+		stringArr.forEach((letra) => {
+			let isLower = false
+
+			const check_letter = letra.charCodeAt()
+			
+			if (check_letter >= 97 && check_letter <= 122) {
+				isLower = true
+			}
+			
+			const asciiNum = letra.toUpperCase().charCodeAt();
+
+			const code = (asciiNum + 65 - parseInt(offset)) % 26 + 65;
+
+			if(isLower){
+				solved += String.fromCharCode(code).toLowerCase()
+			}else if(check_letter === 32){
+				solved += ' ';
+			}else{
+				solved += String.fromCharCode(code)
 			}
 		});
 		return solved;
