f0e4eafe1e849de7303e460f42a8b53727900d3e
marcelimxo <marcelimrondon@gmail.com>
2019-06-13 18:05:39 -0500

    Agregue pantalla final y boton salir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 //const checkLogin = document.getElementById('checkLogin');
-const pswrd = document.getElementById("pswrd");
-const form = document.getElementById("form");
-const correct_pswrd = "LABORATORIA";
+const pswrd = document.getElementById('pswrd');
+const form = document.getElementById('form');
+const correct_pswrd = 'LABORATORIA';
+
+const opt = document.getElementById('op');
+const encodeSection = document.getElementById('cifrando');
+const decodeSection = document.getElementById('descifrando');
+const final = document.getElementById('final');
 
 //contador de errores
 let fails = 0;
@@ -10,71 +15,66 @@ const check = e => {
   e.preventDefault();
   if (pswrd.value.length > 0) {
     if (pswrd.value === correct_pswrd) {
-      const loginContainer = document.getElementById("loginContainer");
+      const loginContainer = document.getElementById('loginContainer');
       loginContainer.remove();
-      const description = document.getElementById("description");
-      description.classList.add("hide");
-      const opt = document.getElementById("op");
-      opt.classList.replace("hide", "show");
+      const description = document.getElementById('description');
+      description.classList.add('hide');
+      opt.classList.replace('hide', 'show');
     } else {
-      pswrd.classList.add("error");
+      pswrd.classList.add('error');
 
       if (fails < 2) {
         // si tiene menos de 3 intentos entra aqui
 
-        const error_pswrd = document.getElementById("error_pswrd");
-        error_pswrd.classList.replace("hide", "show");
+        const error_pswrd = document.getElementById('error_pswrd');
+        error_pswrd.classList.replace('hide', 'show');
 
         // borramos la data del input
-        pswrd.value = "";
+        pswrd.value = '';
 
         // sumamos +1 al contador de errores
         fails++;
       } else {
         // al tercer fallo entra aqui
-        const modal = document.getElementById("error");
-        const overlay = document.getElementById("overlay");
+        const modal = document.getElementById('error');
+        const overlay = document.getElementById('overlay');
 
-        modal.classList.replace("hide", "show");
-        overlay.classList.replace("hide", "show");
+        modal.classList.replace('hide', 'show');
+        overlay.classList.replace('hide', 'show');
       }
     }
   }
 };
 
-form.addEventListener("submit", check);
+form.addEventListener('submit', check);
 
 // mostrar y ocultar contraseña
 
 const eye = document.getElementById('eye');
 
-const togglePswrd = ()=>{
-  
+const togglePswrd = () => {
   eye.classList.toggle('eyeAct');
 
-  (pswrd.type === 'password') ? pswrd.type = 'text' : pswrd.type = 'password';
-
+  pswrd.type === 'password' ? (pswrd.type = 'text') : (pswrd.type = 'password');
 };
 
-eye.addEventListener('click',togglePswrd);
+eye.addEventListener('click', togglePswrd);
 
 //opciones
-const options = document.getElementById("options");
-options.addEventListener("change", e => {
-  const encodeSection = document.getElementById("cifrando");
-  const decodeSection = document.getElementById("descifrando");
-  if (e.target.value === "Cifrar") {
-    encodeSection.classList.replace("hide", "show");
-    decodeSection.classList.replace("show", "hide");
-    textoCifrar.value = "";
-    textoResultadoCifrar.value = "";
+const options = document.getElementById('options');
+options.addEventListener('change', e => {
+  if (e.target.value === 'Cifrar') {
+    encodeSection.classList.replace('hide', 'show');
+    decodeSection.classList.replace('show', 'hide');
+    textoCifrar.value = '';
+    textoResultadoCifrar.value = '';
     rangeCifrar.value = 1;
     fieldCifrar.value = 1;
-  } else if (e.target.value === "Descifrar") {
-    decodeSection.classList.replace("hide", "show");
-    encodeSection.classList.replace("show", "hide");
-    textoDescifrar.value = "";
-    textoResultadoDescifrar.value = "";
+  } else if (e.target.value === 'Descifrar') {
+    decodeSection.classList.replace('hide', 'show');
+    encodeSection.classList.replace('show', 'hide');
+    textoDescifrar.value = '';
+    textoResultadoDescifrar.value = '';
     rangeDescifrar.value = 1;
     fieldDescifrar.value = 1;
   }
@@ -84,70 +84,82 @@ options.addEventListener("change", e => {
 
 /* slider */
 
-const rangeCifrar = document.querySelector("#rangeCifrar");
-const fieldCifrar = document.getElementById("numCifrar");
+const rangeCifrar = document.querySelector('#rangeCifrar');
+const fieldCifrar = document.getElementById('numCifrar');
 
-rangeCifrar.addEventListener("input", e => {
+rangeCifrar.addEventListener('input', e => {
   fieldCifrar.value = e.target.value;
 });
-fieldCifrar.addEventListener("input", e => {
+fieldCifrar.addEventListener('input', e => {
   rangeCifrar.value = e.target.value;
 });
 
 /* Cifrando y mostrando */
 
-const textoCifrar = document.getElementById("textoCifrar");
-const textoResultadoCifrar = document.getElementById("textoResultadoCifrar");
+const textoCifrar = document.getElementById('textoCifrar');
+const textoResultadoCifrar = document.getElementById('textoResultadoCifrar');
 
-const btnCifrar = document.getElementById("cifrar");
-btnCifrar.addEventListener("click", () => {
-  const textoCifrado = window.cipher.encode(fieldCifrar.value,textoCifrar.value);
+const btnCifrar = document.getElementById('cifrar');
+btnCifrar.addEventListener('click', () => {
+  const textoCifrado = window.cipher.encode(
+    fieldCifrar.value,
+    textoCifrar.value
+  );
   textoResultadoCifrar.value = textoCifrado;
 });
 
 /* reset en cifrar */
 
-const btnResetCifrar = document.getElementById("resetCifrar");
-btnResetCifrar.addEventListener("click", () => {
-  textoCifrar.value = "";
+const btnResetCifrar = document.getElementById('resetCifrar');
+btnResetCifrar.addEventListener('click', () => {
+  textoCifrar.value = '';
   rangeCifrar.value = 1;
   fieldCifrar.value = 1;
 });
 
 /* copiar en cifrar */
 
-const btnCopiarCifrado = document.getElementById("copiarCifrado");
-btnCopiarCifrado.addEventListener("click", () => {
+const btnCopiarCifrado = document.getElementById('copiarCifrado');
+btnCopiarCifrado.addEventListener('click', () => {
   textoResultadoCifrar.select();
-  document.execCommand("Copy");
+  document.execCommand('Copy');
 
-  const copiar = document.getElementById("copiarCifrado_span");
-  copiar.classList.toggle("hide");
+  const copiar = document.getElementById('copiarCifrado_span');
+  copiar.classList.toggle('hide');
 });
+
+/* boton salir en cifrar */
+
+const salirCifrar = document.getElementById('salirCifrar');
+salirCifrar.addEventListener('click', ()=>{
+  encodeSection.classList.replace('show','hide');
+  opt.classList.replace('show','hide');
+  final.classList.replace('hide','show');
+})
 
 // sección descifrar
 
 /* slider */
 
-const rangeDescifrar = document.querySelector("#rangeDescifrar");
-const fieldDescifrar = document.getElementById("numDescifrar");
+const rangeDescifrar = document.querySelector('#rangeDescifrar');
+const fieldDescifrar = document.getElementById('numDescifrar');
 
-rangeDescifrar.addEventListener("input", e => {
+rangeDescifrar.addEventListener('input', e => {
   fieldDescifrar.value = e.target.value;
 });
-fieldDescifrar.addEventListener("input", e => {
+fieldDescifrar.addEventListener('input', e => {
   rangeDescifrar.value = e.target.value;
 });
 
 /* Descifrando y mostrando */
 
-const textoDescifrar = document.getElementById("textoDescifrar");
+const textoDescifrar = document.getElementById('textoDescifrar');
 const textoResultadoDescifrar = document.getElementById(
-  "textoResultadoDescifrar"
+  'textoResultadoDescifrar'
 );
 
-const btnDescifrar = document.getElementById("descifrar");
-btnDescifrar.addEventListener("click", () => {
+const btnDescifrar = document.getElementById('descifrar');
+btnDescifrar.addEventListener('click', () => {
   const textoDescifrado = window.cipher.decode(
     fieldDescifrar.value,
     textoDescifrar.value
@@ -158,20 +170,29 @@ btnDescifrar.addEventListener("click", () => {
 
 /* reset en descifrar */
 
-const btnResetDescifrar = document.getElementById("resetDescifrar");
-btnResetDescifrar.addEventListener("click", () => {
-  textoDescifrar.value = "";
+const btnResetDescifrar = document.getElementById('resetDescifrar');
+btnResetDescifrar.addEventListener('click', () => {
+  textoDescifrar.value = '';
   rangeDescifrar.value = 1;
   fieldDescifrar.value = 1;
 });
 
-/* copiar en cifrar */
+/* copiar en descifrar */
 
-const btnCopiarDescifrado = document.getElementById("copiarDescifrado");
-btnCopiarDescifrado.addEventListener("click", () => {
+const btnCopiarDescifrado = document.getElementById('copiarDescifrado');
+btnCopiarDescifrado.addEventListener('click', () => {
   textoResultadoDescifrar.select();
-  document.execCommand("Copy");
+  document.execCommand('Copy');
 
-  const copiar = document.getElementById("copiarDescifrado_span");
-  copiar.classList.toggle("hide");
+  const copiar = document.getElementById('copiarDescifrado_span');
+  copiar.classList.toggle('hide');
 });
+
+/* boton salir en descifrar */
+
+const salirDescifrar = document.getElementById('salirDescifrar');
+salirDescifrar.addEventListener('click', ()=>{
+  decodeSection.classList.replace('show','hide');
+  opt.classList.replace('show','hide');
+  final.classList.replace('hide','show');
+})
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -14,8 +14,12 @@ describe('cipher', () => {
       assert.equal(cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),'HIJKLMNOPQRSTUVWXYZABCDEFG')
     })
 
-    it('Debe retornar "H" cuando ingrese la letra "A" con el offset 33',()=>{
-      assert.equal(cipher.encode(33,'A'),'H')
+    it('Debe retornar " " cuando ingrese " " con el offset 26',()=>{
+      assert.equal(cipher.encode(26,' '),' ')
+    })
+
+    it('Debe retornar "Bb" si ingresa "A.a" con offset 1',()=>{
+      assert.equal(cipher.encode(1,'A.a'),'Bb')
     })
 
     it('deberia retornar "Ovsh Sbsb" para "Hola Lulu" con offset 33',()=>{
@@ -32,6 +36,10 @@ describe('cipher', () => {
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33',()=>{
       assert.equal(cipher.decode(33,'HIJKLMNOPQRSTUVWXYZABCDEFG'),'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     });
+
+    it('Debe retornar " " cuando ingrese " " con el offset 26',()=>{
+      assert.equal(cipher.decode(26,' '),' ')
+    })
 
     it('deberia retornar "Hola Lulu" para "Ovsh Sbsb" con offset 33',()=>{
       assert.equal(cipher.decode(33,'Ovsh Sbsb'),'Hola Lulu')
